0a441d49c8055a1e9ac541f11c9a18ba68f49129
ozanteoman <ozanteomandayanan@gmail.com>
2019-11-17 13:29:23 +0300

    added id to Service Serializer

--- project_app/serializers/service.py
+++ project_app/serializers/service.py
@@ -7,4 +7,4 @@ class ServiceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Service
-        fields = ["title", "price","slug"]
+        fields = ["id", "title", "price", "slug"]
