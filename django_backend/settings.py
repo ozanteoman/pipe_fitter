b934386851e41f6860cd28c317fe4281d2901d47
ozanteoman <ozanteomandayanan@gmail.com>
2019-11-17 12:46:53 +0300

    fixed cors-header

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -72,9 +72,9 @@ ACCOUNT_UNIQUE_EMAIL = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
