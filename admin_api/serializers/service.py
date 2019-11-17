a0cea8b5431ef5aa5e34a7b79ea1d9310a857160
ozanteoman <ozanteomandayanan@gmail.com>
2019-11-17 13:24:22 +0300

    slug for admin serializer

--- admin_api/serializers/service.py
+++ admin_api/serializers/service.py
@@ -4,7 +4,8 @@ from project_app.models import Service
 
 class ServiceSerializer(serializers.ModelSerializer):
     status = serializers.ReadOnlyField()
+    slug = serializers.SlugField(read_only=True)
 
     class Meta:
         model = Service
-        fields = ['id','title', 'price', 'status', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'slug', 'price', 'status', 'created_at', 'updated_at']
